9b75721d834db059e9c2d21ac8633e02172891e3
Jitrin Siriwat <jitrin@foxicle.com>
2013-06-24 11:45:12 +0700

    update CalcTH

--- ThaiCalculator/src/com/foxdev/thaicalculator/CalcTH.java
+++ ThaiCalculator/src/com/foxdev/thaicalculator/CalcTH.java
@@ -2,12 +2,34 @@ package com.foxdev.thaicalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.GridView;
 
 public class CalcTH extends Activity {
+	
+	GridView gridKeypad;
+	ButtonAdapter buttonAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.thai_view);
 		
+		gridKeypad = (GridView) findViewById(R.id.gridKeypad);
+		
+		buttonAdapter = new ButtonAdapter(this);
+		
+		gridKeypad.setAdapter(buttonAdapter);
+		
+		gridKeypad.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		});
 	}
 }
